cbbe3eda9a2212eaee883b1c3ca892162460ebed
Colt Gaskill <gaskillcw@gmail.com>
2021-09-29 14:08:04 -0400

    edits to home controller for logging in (incomplete)

--- SafeFutureWebApplication/Controllers/HomeController.cs
+++ SafeFutureWebApplication/Controllers/HomeController.cs
@@ -22,14 +22,44 @@ namespace SafeFutureWebApplication.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IAuthService _authService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IAuthService authService, TempDB tempDB)
         {
             _logger = logger;
+            _authService = authService;
         }
 
+        [Authorize("staff")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            ViewData["ErrorMessage"] = null;
+            User user = _authService.ValidateLogin(login);
+            if (user is null)
+            {
+                ViewData["ErrorMessage"] = "Username or password is incorrect.";
+                return View();
+            }
+
+            ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.Username));
+            foreach (string role in user.Role)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, role));
+            }
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+            HttpContext.Session.SetString("SessionKey", login.Username);
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
