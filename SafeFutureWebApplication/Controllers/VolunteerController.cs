bf9e750c2c393dc450c71d45221253bd2eb3b1bf
jpeters743 <82973963+jpeters743@users.noreply.github.com>
2021-09-29 17:00:09 -0400

    Initial add volunteer

---
+++ SafeFutureWebApplication/Controllers/VolunteerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafeFutureWebApplication.Models;
+
+
+namespace SafeFutureWebApplication.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Volunteer _volunteer)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ SafeFutureWebApplication/Models/Volunteer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafeFutureWebApplication.Models
+{
+    public class Volunteer
+    {
+        public string userID { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+    }
+}
