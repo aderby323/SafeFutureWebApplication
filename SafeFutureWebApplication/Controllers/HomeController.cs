49864cc15b2406b1409fb5b61c356d77b6c97dd7
Colt Gaskill <gaskillcw@gmail.com>
2021-09-29 13:29:23 -0400

    Added several documents and folders pertaining to logging in (incomplete)

--- SafeFutureWebApplication/Controllers/HomeController.cs
+++ SafeFutureWebApplication/Controllers/HomeController.cs
@@ -6,12 +6,21 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using SafeFutureWebApplication.Models.ViewModels;
+using SafeFutureWebApplication.Services.Interfaces;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using SafeFutureWebApplication.Repository;
+using Microsoft.AspNetCore.Authentication;
 
 namespace SafeFutureWebApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IAuthService _authService;
 
         public HomeController(ILogger<HomeController> logger)
         {
---
+++ SafeFutureWebApplication/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafeFutureWebApplication.Models;
+using SafeFutureWebApplication.Models.ViewModels;
+using SafeFutureWebApplication.Repository;
+using SafeFutureWebApplication.Services.Interfaces;
+
+namespace SafeFutureWebApplication.Services
+{
+    public class AuthService : IAuthService
+    {
+        private TempDB _tempDB;
+
+        public AuthService(TempDB tempDB)
+        {
+            _tempDB = tempDB;
+        }
+
+        // COLT COME BACK HERE
+
+    }
+}
