ad97a3982d35b79d509130b9b2043548470ad2a7
Alexander Derby <n01037933@unf.edu>
2021-09-30 23:52:01 -0400

    Added participant creation form

--- SafeFutureWebApplication/Controllers/VolunteerController.cs
+++ SafeFutureWebApplication/Controllers/VolunteerController.cs
@@ -2,31 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SafeFutureWebApplication.Models.ViewModels;
+using SafeFutureWebApplication.Models;
+using SafeFutureWebApplication.Repository;
 
 namespace SafeFutureWebApplication.Controllers
 {
+    [Authorize("Admin")]
     public class VolunteerController : Controller
     {
+        private readonly TempDB provider;
+
+        public VolunteerController(TempDB provider)
+        {
+            this.provider = provider;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
         [HttpPost]
-        public IActionResult Register(ParicipantFormViewModel viewModel)
+        public IActionResult Index(Participant participant)
         {
             if (!ModelState.IsValid)
             {
-                return View("Form not fully filled out.");
+                return View(participant);
             }
+
+            participant.ParticipantId = Guid.NewGuid();
+            provider.Participants.Add(participant);
 
             return View();
         }
--- SafeFutureWebApplication/Models/Participant.cs
+++ SafeFutureWebApplication/Models/Participant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,29 @@ namespace SafeFutureWebApplication.Models
     public class Participant
     {
         public Guid ParticipantId { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "First name must not exceed 150 characters.")]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "Last name must not exceed 150 characters.")]
         public string LastName { get; set; }
+
+        [StringLength(200, ErrorMessage = "Address must not exceed 200 characters.")]
         public string Address { get; set; }
+
+        [StringLength(200, ErrorMessage = "Address 2 must not exceed 200 characters.")]
+        public string Address2 { get; set; }
+
+        [StringLength(200, ErrorMessage = "City must not exceed 200 characters.")]
+        public string City { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
         public string ZipCode { get; set; }
+
+        [Required]
         public int HouseholdSize { get; set; }
     }
 }
