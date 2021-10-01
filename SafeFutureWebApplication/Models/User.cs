edb412e415da4e37f8d256fd8479f009b527c2b8
jpeters743 <82973963+jpeters743@users.noreply.github.com>
2021-10-01 11:01:12 -0400

    Added error messages and create to nav bar

--- SafeFutureWebApplication/Models/User.cs
+++ SafeFutureWebApplication/Models/User.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SafeFutureWebApplication.Models
 {
     public class User
     {
+        [Required(ErrorMessage = "Please enter a username")]
         public string Username { get; set; }
+        [Required(ErrorMessage ="Please enter a password")]
         public string Password { get; set; }
+        [Required(ErrorMessage ="Please select a role")]
         public string Role { get; set; }
 
     }
