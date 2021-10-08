28f84947bdd7ad64295eaefd16988a6db7e85a72
Alexander Derby <n01037933@unf.edu>
2021-10-08 16:09:12 -0400

    Refactored TempDb with new Customer fields

--- SafeFutureWebApplication/Models/Customer.cs
+++ SafeFutureWebApplication/Models/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SafeFutureWebApplication.Models
 {
@@ -9,11 +10,28 @@ namespace SafeFutureWebApplication.Models
             ProductsDistributed = new List<string>();
         }
 
-        public string Name { get; set; }
+        // public string Name { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(150, ErrorMessage = "First name must not exceed 150 characters.")]
+        public string FirstName { get; set; }
 
-        public string Zipcode { get; set; }
+        [StringLength(150, ErrorMessage = "Middle name must not exceed 150 characters.")]
+        public string MiddleName { get; set; }
 
-        public string HouseholdSize { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(150, ErrorMessage = "Last name must not exceed 150 characters.")]
+        public string LastName { get; set; }
+
+        // public string Zipcode { get; set; }
+        [Required(ErrorMessage = "Zip code is required.")]
+        [DataType(DataType.PostalCode)]
+        public string ZipCode { get; set; }
+
+        // public string HouseholdSize { get; set; }
+        [Required(ErrorMessage = "Household size is required.")]
+        [Range(1, 15, ErrorMessage = "Householde size range is only from 1 to 15.")]
+        public int HouseholdSize { get; set; }
+
         public string Email { get; set; }
         public List<string> ProductsDistributed { get; set; }
 
