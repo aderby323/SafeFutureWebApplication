8a040928491cf494c9c2453a6b084b3d5c07baf8
Alexander Derby <n01037933@unf.edu>
2021-12-06 17:28:17 -0500

    Setup EF Core scaffold

---
+++ SafeFutureWebApplication/Repository/Models/SFFDatabaseContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace SafeFutureWebApplication.Repository.Models
+{
+    public partial class SFFDatabaseContext : DbContext
+    {
+        public SFFDatabaseContext()
+        {
+        }
+
+        public SFFDatabaseContext(DbContextOptions<SFFDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:sffserver1.database.windows.net;Initial Catalog=SFFDatabase;Persist Security Info=False;User ID=serveradmin;Password=LTc,@65z;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Role)
+                    .IsRequired()
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salt)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ SafeFutureWebApplication/Repository/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace SafeFutureWebApplication.Repository.Models
+{
+    public partial class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public string Role { get; set; }
+    }
+}
