0169fba340813197965450b997b3527a8c9cbc63
Alexander Derby <aderby323@gmail.com>
2021-12-10 22:29:10 -0500

    startup change

--- SafeFutureWebApplication/Startup.cs
+++ SafeFutureWebApplication/Startup.cs
@@ -65,15 +65,7 @@ namespace SafeFutureWebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
